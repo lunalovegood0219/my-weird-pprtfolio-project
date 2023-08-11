b7e09663ddf1003828d3cc07f3495468fa2c5d63
lunalovegood0219 <129015679+lunalovegood0219@users.noreply.github.com>
2023-08-11 22:09:29 +0330

    remove the loop from js

--- app/app.js
+++ app/app.js
@@ -1,13 +1,9 @@
 var typed = new Typed("#atou-type", {
   strings: [
-    `netflix horro series...`,
-    `netflix horro series...`,
-    `netflix horro series...`,
-    `netflix horro series...`,
     `netflix horro series...`
   ],
   typeSpeed: 150,
   backSpeed: 100,
-  loop: true,
+  //loop: true,
 });
 
